0562e9808f8cdbec4fd63df400b08a4db2be4768
gupascutti@uninove.edu.br <gupascutti@uninove.edu.br>
2020-11-18 19:36:26 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ConsultaCEP.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsultaCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TO DO - Lógica do programa
+
+            //Validações.
+            string cep = CEP.Text.Trim(); //trim removo todo espaço do inicio e fim caso seja espaço em branco(vazio)
+            //chama o metodo direto porque criamos um metodo static
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = string.Format("Endereço: {2} Bairro: {3} - {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+        }
+    }
+}
---
+++ ConsultaCEP/ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ConsultaCEP.Servico.Modelo
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep); 
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            //deserealizando esse json conteudo em um objeto do tipo endereço
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
